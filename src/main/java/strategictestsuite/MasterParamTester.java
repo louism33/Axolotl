180d0d293230513b2b57db73abba453be67bf024
Louis <louis.mackenziesmith@googlemail.com>
2019-08-04 23:52:55 +0200

    bug in aspiration

--- src/main/java/strategictestsuite/MasterParamTester.java
+++ src/main/java/strategictestsuite/MasterParamTester.java
@@ -4,7 +4,7 @@ import com.github.louism33.axolotl.search.Engine;
 
 public class MasterParamTester {
 
-    public static boolean printBoard = true, printFen = true, enableAssert = true, printMyMove = true;
+    public static boolean printBoard = false, printFen = false, enableAssert = false, printMyMove = false;
     
 //    public static final int timeLimit = 10_000;
     public static final int timeLimit = 1_000;
